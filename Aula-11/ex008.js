6bde22ff92ffd12f40abd2cc9f47fc3d46eecdae
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-10 23:30:17 -0300

    Create ex008.js
    exercicio de condicao

---
+++ Aula-11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80.5
+console.log(`O seu carro está a ${vel}`)
+if (vel >60) {
+    console.log(`Voce passou acima da velocidade permitida  
+          * MULTADO *`)}
+console.log(`dirija sempre com cuidado !`)
