f1220bdcefa2e7dc3ab4d2b79b9fc7ba704a85b2
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-11 22:54:43 -0300

    Create ex011.js

---
+++ aula-12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 14
+if (idade <16) {
+console.log(`NÃO VOTA`)
+}else {
+    if (idade >= 16 && idade <18){
+        console.log(`VOTO OPCIONAL`)    
+    }
+    else{
+        console.log('VOTA')
+    }
+}
