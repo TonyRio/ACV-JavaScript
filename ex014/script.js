ddd7af4d2028563700ef1060c78cf28c081bc648
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-13 20:39:49 -0300

    monitor de dia

---
+++ ex014/script.js
@@ -0,0 +1,28 @@
+function carregar(){
+    var msg = document.getElementById(`msg`)
+    var img = document.getElementById(`imagem`)
+    var data = new Date()
+    var hora = data.getHours()
+    var hora =22
+  
+   msg.innerHTML = `agora são ${hora} horas.`
+    
+    if (hora >= 0 && hora <12){
+        //BOM DIA 
+        img.src = `fotomanha.png`}
+        
+        
+      
+    else if (hora >= 12 && hora <18) {
+            //BOA TARDE 
+        img.src = `fototarde.png`
+        document.body.style.background = "#b9846f"
+    }
+    else {
+            //BOA NOITE 
+        img.src = `fotonoite.png`
+        document.body.style.background = "#515154"
+    }
+    }
+
+    
