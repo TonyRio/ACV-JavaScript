4ff83bdfd23f53f7b03651e8a149326947090460
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-14 23:58:30 -0300

    Update ambiente.js

--- aula-12/aula-13/ambiente.js
+++ aula-12/aula-13/ambiente.js
@@ -1,5 +1,15 @@
 var c =1
-while (c<6){
-console.log(`${c} - tudo bem ?`)
+do { (c<30)
+console.log(`passo ${c} - tudo bem ?`)
 c++
 }
+while (c<=10)
+
+
+
+
+/*var c =1
+while (c<30){
+console.log(`passo ${c} - tudo bem ?`)
+c++
+}*/
