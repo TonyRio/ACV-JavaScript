15e7e8cc35bf8da91a4b2395bf2f6234fc3c2b44
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-15 00:40:46 -0300

    debug de script

--- aula -14/ambiente.js
+++ aula -14/ambiente.js
@@ -1,9 +1,5 @@
-/*var c =1
-while (c<=5){
-console.log(c)
-c++
-
-}*/
-for(var c =1; c<=15; c++){
-    console.log(c)
+console.log("Vai começar....")
+for(var c =1; c<=10; c++){
+   console.log(c)
 }
+console.log("Terminou !")
