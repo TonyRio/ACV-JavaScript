5cfe55a0f1e6cfdfdf0a2096bdcd5e9b75d6dff6
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-17 00:30:55 -0300

    Update script.js

--- aula-14-ex/ex016/script.js
+++ aula-14-ex/ex016/script.js
@@ -8,14 +8,27 @@ function contar() {
     if (ini.value.length ==0 || fim.value.length ==0 || passo.value.length ==0){
     alert (' [ERRO] - nao podemos prosseguir sem os dados') }
     else {
-        res.innerHTML = `contando: `
+        res.innerHTML = `contando: <br><br>`
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p==0){
+            alert('Passo Invalido, Contando de 1 em 1')
+            p=1
+        }
         
-        for (let c =i ; c <= f; c += p){
-            res.innerHTML += `${c} `
+        // contagem progressiva
+        if (i <=f)
+            for (let c =i ; c <= f; c += p){
+                res.innerHTML += `${c}  \u{1F449}`}
+        // contagem regressiva
+        else{
+            for (let c =i; c>=f; c-=p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
 
         }
+        
+        res.innerHTML += `\u{1F6D1}`
     }
 }
