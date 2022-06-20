c3160fbe6a5a8a40ff95e032b123e4129363f92b
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-20 15:48:20 -0300

    Update ambiente.js

--- aula-15/ambiente.js
+++ aula-15/ambiente.js
@@ -1,7 +1,11 @@
 let num = [3,45,6,32,2,31]
-num[12]=37
+num[5]=37
 num.push(76)
 num.push(30)
-console.log(num.length)
+console.log(`o Vetor tem ${num.length} posiçoes `)
 
 console.log(`nosso vetor é ${num}`)
+console.log(`nosso vetor em ordem crrescente  ${num.sort()}`)
+for (let pos =0;pos<num.length;pos++ ){
+    console.log(`a poscicao ${pos} tem o valor ${num.sort()[pos]}`)
+}
