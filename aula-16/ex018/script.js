9c965d6ac5bef72c5e2981b372df1e88b5700d85
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-23 15:10:27 -0300

    correcao

--- aula-16/ex018/script.js
+++ aula-16/ex018/script.js
@@ -3,8 +3,27 @@ let lista =document.querySelector("select#flista")
 let res = document.querySelector("div#res")
 let valores = []
 
+function isNumero(n){
+if (Number(n) >=1 && Number(n) <=100){
+    return true
+}else {
+        return false
+}
+}
+
+
+function inLista(n, l){
+if (l.indexOf(Number(n)) != -1){
+    return true
+}else {
+        return false
+}
+}
+
+
 function adicionar(){
-    if (isNumero(num.value)&& !inLista(num.value)){
+    if (isNumero(num.value)&& !inLista(num.value, valores)){
+        alert("Tudo OK")
     }
     else {
         alert(`Valor Inválido ou já cadastrado. `)
