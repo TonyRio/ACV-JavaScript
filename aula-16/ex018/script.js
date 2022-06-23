43e86425e2912199770df2474452b3b425b1d5d0
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-23 16:05:41 -0300

    soma e media

--- aula-16/ex018/script.js
+++ aula-16/ex018/script.js
@@ -11,23 +11,61 @@ if (Number(n) >=1 && Number(n) <=100){
 }
 }
 
-
 function inLista(n, l){
 if (l.indexOf(Number(n)) != -1){
+    
     return true
 }else {
         return false
 }
 }
 
-
 function adicionar(){
     if (isNumero(num.value)&& !inLista(num.value, valores)){
-        alert("Tudo OK")
+        valores.push(Number(num.value))
+        let item = document.createElement("option")
+        item.text = `Valor ${num.value} foi adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ""
+        //alert("Tudo OK")
+        
     }
     else {
         alert(`Valor Inválido ou já cadastrado. `)
     }
-
+    num.value =""
+    num.focus()
 }
-  
+   
+function finalizar() {
+    if (valores.length == 0){
+    alert(`adicione Valores pra prosseguir`)
+    } 
+    else{
+    let tot = valores.length    
+    let maior =valores[0]
+    let menor =valores[0]
+    let soma =0
+    let media =0
+    for (let pos in valores){
+        soma += valores[pos]
+      
+        if (valores[pos] > maior)
+            maior = valores[pos]
+        if (valores[pos]< menor) 
+            menor = valores[pos]
+        }
+   
+        res.innerHTML =""
+        res.innerHTML += `<p> Ao todo temos ${tot} Numeros cadastrados.</p>`
+        res.innerHTML +=  `<p> o MAIOR numero é ${maior} </p>`
+        res.innerHTML +=  `<p> o MENOR numero é ${menor} </p>`
+        res.innerHTML +=  `<p> Somando todos os valores dará: ${soma} </p>`
+    }
+    }
+   
+    
+
+    
+
+
